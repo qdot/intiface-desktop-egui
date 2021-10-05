6935df46f763cddc290ed35852a03197773127f7
Kyle Machulis <kyle@nonpolynomial.com>
2021-10-05 00:06:52 -0700

    chore: Add deserialization for EngineLogMessage

--- src/core/process_messages.rs
+++ src/core/process_messages.rs
@@ -1,4 +1,7 @@
 use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
+use std::convert::TryFrom;
+use std::str::FromStr;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum EngineMessage {
@@ -19,9 +22,81 @@ pub enum IntifaceMessage {
   Stop
 }
 
-/*
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct EngineLogMessage {
-  
+  #[serde(default)]
+  timestamp: String,
+  #[serde(default)]
+  level: String,
+  #[serde(default)]
+  fields: EngineLogMessageFields,
+  #[serde(default)]
+  target: String
 }
-*/
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+pub struct EngineLogMessageFields {
+  message: String,
+  #[serde(rename = "log.module_path", default)]
+  module_path: String,
+  #[serde(rename = "log.file", default)]
+  file: String,
+  #[serde(rename = "log.line", default)]
+  line: u32,
+}
+
+impl EngineLogMessage {
+  pub fn log_event(&self) {
+    let span = tracing::span!(tracing::Level::ERROR, "intiface_cli");
+    let _enter = span.enter();
+    let level = tracing::Level::from_str(&self.level).unwrap();
+    match level {
+      tracing::Level::ERROR => {
+        tracing::error!(
+          engine_target = ?self.target,
+          engine_file = ?self.fields.file,
+          engine_line = self.fields.line,
+          engine_module_path = ?self.fields.module_path,
+          "{}", self.fields.message
+        );
+      }
+      tracing::Level::WARN => {
+        tracing::warn!(
+          engine_target = ?self.target,
+          engine_file = ?self.fields.file,
+          engine_line = self.fields.line,
+          engine_module_path = ?self.fields.module_path,
+          "{}", self.fields.message
+        );
+      }
+      tracing::Level::INFO => {
+        tracing::info!(
+          engine_target = ?self.target,
+          engine_file = ?self.fields.file,
+          engine_line = self.fields.line,
+          engine_module_path = ?self.fields.module_path,
+          "{}", self.fields.message
+        );
+      }
+      tracing::Level::DEBUG => {
+        tracing::debug!(
+          engine_target = ?self.target,
+          engine_file = ?self.fields.file,
+          engine_line = self.fields.line,
+          engine_module_path = ?self.fields.module_path,
+          "{}", self.fields.message
+        );
+      }
+      tracing::Level::TRACE => {
+        tracing::trace!(
+          engine_target = ?self.target,
+          engine_file = ?self.fields.file,
+          engine_line = self.fields.line,
+          engine_module_path = ?self.fields.module_path,
+          "{}", self.fields.message
+        );
+
+      }
+    };
+  }
+}
