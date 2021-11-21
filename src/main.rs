749d042570c24816df400a085f569db2b7b14df1
Kyle Machulis <kyle@nonpolynomial.com>
2021-11-20 21:17:51 -0800

    chore: Add icon/installer images

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,27 @@
 #[macro_use]
 extern crate tracing;
 
+use eframe::epi::IconData;
+use std::io::Cursor;
+
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 #[tokio::main]
 async fn main() {
   info!("Starting application");
+  let app_icon = include_bytes!("../icons/intiface-desktop-logo.ico");
   let app = intiface_desktop_egui::IntifaceDesktopApp::default();
-  let native_options = eframe::NativeOptions::default();
+  let mut native_options = eframe::NativeOptions::default();
+  let reader = Cursor::new(app_icon);
+  let icon_dir = ico::IconDir::read(reader).unwrap();
+  // Decode the first entry into an image:
+  let image = icon_dir.entries()[2].decode().unwrap();
+  // You can get raw RGBA pixel data to pass to another image library:
+  let rgba = image.rgba_data();
+  native_options.icon_data = Some(IconData {
+    rgba: rgba.to_vec(),
+    width: image.width(),
+    height: image.height()
+  });
   eframe::run_native(Box::new(app), native_options);
 }
