22bf3370e591028536e61a072b27181174251975
Kyle Machulis <kyle@nonpolynomial.com>
2022-01-17 16:56:14 -0800

    chore: Device test panel should allow for per-actuator control

--- src/panels/device_test_panel.rs
+++ src/panels/device_test_panel.rs
@@ -9,23 +9,35 @@ use buttplug::{
 use eframe::egui;
 use futures::StreamExt;
 use sentry::SentryFutureExt;
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 use tracing::info;
 
 #[derive(Default)]
 pub struct DeviceTestPanel {
-  client: Option<Arc<ButtplugClient>>
+  client: Option<Arc<ButtplugClient>>,
+  // Store the "all" value and each individual actuator value in a tuple.
+  vibrate_values: HashMap<u32, (f64, Vec<f64>)>,
+  rotate_values: HashMap<u32, (f64, Vec<f64>)>,
+  linear_values: HashMap<u32, ((f64, f64), Vec<(f64, f64)>)>,
 }
 
 impl DeviceTestPanel {
   pub fn update(&mut self, core: &mut AppCore, ui: &mut egui::Ui) {
-    if core.process_manager.is_running() && (core.process_manager.client_name().is_none() || self.client.is_some()) {
+    if core.process_manager.is_running()
+      && (core.process_manager.client_name().is_none() || self.client.is_some())
+    {
       if self.client.is_some() {
-        let client = self.client.as_ref().expect("Already checked existence").clone();
+        let client = self
+          .client
+          .as_ref()
+          .expect("Already checked existence")
+          .clone();
+        
         if !client.connected() {
           ui.label("Connecting...");
           return;
         }
+
         ui.vertical(|ui| {
           ui.horizontal(|ui| {
             if ui.button("Scan for Devices").clicked() {
@@ -49,67 +61,132 @@ impl DeviceTestPanel {
             }
           });
           for device in client.devices() {
-            ui.collapsing(format!("{}", device.name), |ui| {
+            ui.collapsing(format!("{}", device.name), |ui| 
+            {
               if device
                 .allowed_messages
                 .contains_key(&ButtplugCurrentSpecDeviceMessageType::VibrateCmd)
               {
-                let vibrate_id =
-                  ui.make_persistent_id(format!("DevicesPanel::Vibrate::{}", device.index()));
-                let mut vibrate_value = ui
-                  .memory()
-                  .data
-                  .get_temp_mut_or_default::<f64>(vibrate_id)
-                  .clone();
-                if ui
-                  .add(
-                    egui::Slider::new::<f64>(&mut vibrate_value, 0.0..=1.0).text("Vibration Level"),
-                  )
-                  .changed()
-                {
+                let vibrate_value = self.vibrate_values.entry(device.index()).or_insert((
+                  0f64,
+                  vec![
+                    0f64;
+                    device
+                      .allowed_messages
+                      .get(&ButtplugCurrentSpecDeviceMessageType::VibrateCmd)
+                      .expect("Already tested.")
+                      .feature_count
+                      .unwrap_or(0) as usize
+                  ],
+                ));
+                if vibrate_value.1.len() > 1 {
+                  if ui
+                    .add(
+                      egui::Slider::new::<f64>(&mut vibrate_value.0, 0.0..=1.0)
+                        .text("Vibration Level (All Vibrators)"),
+                    )
+                    .changed()
+                  {
+                    // .count() used to consume iterator.
+                    vibrate_value.1.iter_mut().map(|v| *v = vibrate_value.0).count();
+                    let device_clone = device.clone();
+                    let level = vibrate_value.0;
+                    tokio::spawn(
+                      async move {
+                        device_clone.vibrate(VibrateCommand::Speed(level)).await;
+                      }
+                      .bind_hub(sentry::Hub::current().clone()),
+                    );
+                  }
+                }
+                let mut update = false;
+                for (index, speed) in vibrate_value.1.iter_mut().enumerate() {
+                  if ui
+                    .add(
+                      egui::Slider::new::<f64>(speed, 0.0..=1.0)
+                        .text(format!("Vibrator {} Level", index + 1)),
+                    )
+                    .changed()
+                  {
+                    update = true;
+                  }                  
+                }
+                if update {
                   let device_clone = device.clone();
+                  let level = vibrate_value.1.clone();
                   tokio::spawn(
                     async move {
-                      device_clone
-                        .vibrate(VibrateCommand::Speed(vibrate_value))
-                        .await;
+                      device_clone.vibrate(VibrateCommand::SpeedVec(level)).await;
                     }
                     .bind_hub(sentry::Hub::current().clone()),
                   );
                 }
-                ui.memory().data.insert_temp(vibrate_id, vibrate_value);
               }
+
+            
               if device
                 .allowed_messages
                 .contains_key(&ButtplugCurrentSpecDeviceMessageType::RotateCmd)
               {
-                let rotate_id =
-                  ui.make_persistent_id(format!("DevicesPanel::Rotate::{}", device.index()));
-                let mut rotate_value = ui
-                  .memory()
-                  .data
-                  .get_temp_mut_or_default::<f64>(rotate_id)
-                  .clone();
-                if ui
-                  .add(
-                    egui::Slider::new::<f64>(&mut rotate_value, -1.0..=1.0).text("Rotation Level"),
-                  )
-                  .changed()
-                {
+                let rotate_value = self.rotate_values.entry(device.index()).or_insert((
+                  0f64,
+                  vec![
+                    0f64;
+                    device
+                      .allowed_messages
+                      .get(&ButtplugCurrentSpecDeviceMessageType::RotateCmd)
+                      .expect("Already tested.")
+                      .feature_count
+                      .unwrap_or(0) as usize
+                  ],
+                ));
+                if rotate_value.1.len() > 1 {
+                  if ui
+                    .add(
+                      egui::Slider::new::<f64>(&mut rotate_value.0, -1.0..=1.0)
+                        .text("Rotation Level (All Rotators)"),
+                    )
+                    .changed()
+                  {
+                    // .count() used to consume iterator.
+                    rotate_value.1.iter_mut().map(|v| *v = rotate_value.0).count();
+                    let device_clone = device.clone();
+                    let level = rotate_value.0;
+                    tokio::spawn(
+                      async move {
+                        device_clone
+                          .rotate(RotateCommand::Rotate(
+                            level.abs(),
+                            level >= 0f64,
+                          ))
+                          .await;
+                      }
+                      .bind_hub(sentry::Hub::current().clone()),
+                    );
+                  }
+                }
+                let mut update = false;
+                for (index, speed) in rotate_value.1.iter_mut().enumerate() {
+                  if ui
+                    .add(
+                      egui::Slider::new::<f64>(speed, -1.0..=1.0)
+                        .text(format!("Rotator {} Level", index + 1)),
+                    )
+                    .changed()
+                  {
+                    update = true;
+                  }                  
+                }
+                if update {
                   let device_clone = device.clone();
+                  let level = rotate_value.1.clone();
                   tokio::spawn(
                     async move {
-                      device_clone
-                        .rotate(RotateCommand::Rotate(
-                          rotate_value.abs(),
-                          rotate_value < 0f64,
-                        ))
-                        .await;
+                      device_clone.rotate(RotateCommand::RotateVec(level.iter().map(|v| (v.abs(), *v >= 0f64)).collect())).await;
                     }
                     .bind_hub(sentry::Hub::current().clone()),
                   );
                 }
-                ui.memory().data.insert_temp(rotate_id, rotate_value);
               }
             });
           }
